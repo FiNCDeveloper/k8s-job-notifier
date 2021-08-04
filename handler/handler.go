d8f7926b8b223931b4c2426192c660ca57185bd0
sho naito <sho20100@gmail.com>
2021-08-04 13:40:29 +0900

    Merge create handler file

--- handler/handler.go
+++ handler/handler.go
@@ -1,7 +1,47 @@
 package handler
 
-import "github.com/FiNCDeveloper/k8s-job-notifier/event"
+import (
+	"os"
+
+	"github.com/FiNCDeveloper/k8s-job-notifier/event"
+	"github.com/FiNCDeveloper/k8s-job-notifier/slack"
+)
 
 type Handler interface {
 	Handle(e event.Event)
+}
+
+func CreateHandler() (Handler, error) {
+	h, err := createSlackHandler()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return h, nil
+}
+
+func createSlackHandler() (Handler, error) {
+	dc := os.Getenv("DEFAULT_CHANNEL")
+	if len(dc) == 0 {
+		dc = "#bot_sandbox"
+	}
+
+	enabled := false
+	switch s := os.Getenv("SLACK_DEFAULT_ENABLED"); s {
+	case "true":
+		enabled = true
+	case "false":
+		enabled = false
+	default:
+		enabled = false
+	}
+
+	return &slack.Slack{
+		Token:            os.Getenv("SLACK_TOKEN"),
+		DefaultChannel:   dc,
+		Title:            "job notify",
+		NotifyCondisions: []string{"Failed"},
+		DefaultEnabled:   enabled,
+	}, nil
 }
